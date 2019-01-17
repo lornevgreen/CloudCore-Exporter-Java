623383b4beb035366f059cfb409a2e9caf5995a3
SirBenWard <sirbenward@users.noreply.github.com>
2019-01-17 13:58:58 -0800

    Added timer.

--- src/main/java/com/cloudcore/exporter/Main.java
+++ src/main/java/com/cloudcore/exporter/Main.java
@@ -8,48 +8,43 @@ import com.cloudcore.exporter.utils.SimpleLogger;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.time.Instant;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
-import java.util.Arrays;
 
 public class Main {
 
     public static void main(String[] args) {
         SimpleLogger.writeLog("ServantExporterStarted", "");
         ArrayList<Command> commands;
+        if (args.length != 0 && Files.exists(Paths.get(args[0]))) {
+            System.out.println("New root path: " + args[0]);
+            FileSystem.changeRootPath(args[0]);
+        }
 
+        FileSystem.createDirectories();
+
+        commands = FileSystem.getCommands();
+        if (commands.size() > 0)
+            for (Command command : commands) {
+                boolean successful = Exporter.ExportCoins(Exporter.calculateNotesForTotal("", command.amount),
+                        command.type, "", command.tag);
+                FileSystem.archiveCommand(command);
+            }
+
+        FolderWatcher watcher = new FolderWatcher(FileSystem.CommandsFolder);
+        System.out.println("Watching for commands at " + FileSystem.CommandsFolder);
         while (true) {
             try {
-                if (args.length != 0 && Files.exists(Paths.get(args[0]))) {
-                    System.out.println("New root path: " + args[0]);
-                    FileSystem.changeRootPath(args[0]);
-                }
-                FileSystem.createDirectories();
+                Thread.sleep(1000);
 
-                FolderWatcher watcher = new FolderWatcher(FileSystem.CommandsFolder);
-                System.out.println("Watching for commands at " + FileSystem.CommandsFolder);
-                boolean stop = false;
-
-                commands = FileSystem.getCommands();
-                if (commands.size() > 0)
-                    for (Command command : commands) {
-                        boolean successful = Exporter.ExportCoins(Exporter.calculateNotesForTotal("", command.amount),
-                                command.type, "", command.tag);
-                        FileSystem.archiveCommand(command);
-                    }
-
-                while (!stop) {
-                    if (watcher.newFileDetected()) {
-                        System.out.println(Instant.now().toString() + ": Exporting coins...");
-                        commands = FileSystem.getCommands();
-                        if (commands.size() > 0)
-                            for (Command command : commands) {
-                                boolean successful = Exporter.ExportCoins(Exporter.calculateNotesForTotal("", command.amount),
-                                        command.type, "", command.tag);
-                                FileSystem.archiveCommand(command);
-                            }
-                    }
+                if (watcher.newFileDetected()) {
+                    System.out.println(Instant.now().toString() + ": Exporting coins...");
+                    commands = FileSystem.getCommands();
+                    if (commands.size() > 0)
+                        for (Command command : commands) {
+                            boolean successful = Exporter.ExportCoins(Exporter.calculateNotesForTotal("", command.amount),
+                                    command.type, "", command.tag);
+                            FileSystem.archiveCommand(command);
+                        }
                 }
             } catch (Exception e) {
                 System.out.println("Uncaught exception - " + e.getLocalizedMessage());
